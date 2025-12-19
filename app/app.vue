a4af43b73c0534cce575bbcf8ac4d3591cd707e6
Predrag Stojadinovic <predrags@nvidia.com>
2025-12-19 18:07:59 +0100

    chore: initial

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,6 @@
 <template>
-  <NuxtRouteAnnouncer />
-  <NuxtPage />
+	<NuxtLayout>
+		<NuxtRouteAnnouncer/>
+		<NuxtPage/>
+	</NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	devServer: {
+		port: 4000
+	},
 	modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint'],
 	devtools: {enabled: true},
 	compatibilityDate: '2024-04-03'
